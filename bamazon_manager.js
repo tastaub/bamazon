da097c0645574be3447773eeb9cf69466bcb988c
Thomas Staub <thomasstaub@Thomass-MacBook-Pro.local>
2018-06-01 20:26:27 -0400

    promises added for manager task

--- bamazon_manager.js
+++ bamazon_manager.js
@@ -1,56 +1,55 @@
-require('dotenv').config();
-var mysql = require('mysql');
-var inquire = require('inquirer');
-var Table = require('easy-table');
+require("dotenv").config();
+var mysql = require("mysql");
+var inquire = require("inquirer");
+var Table = require("easy-table");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
+  host: "localhost",
+  port: 3306,
 
-    user: 'root',
+  user: "root",
 
-    password: process.env.password,
-    database: "bamazon_DB"
-})
+  password: process.env.password,
+  database: "bamazon_DB"
+});
 
 connection.connect(function(err) {
-    if (err) throw err;
-    managerTask();
-  });
+  if (err) throw err;
+  managerTask();
+});
 
-function managerTask()  {
-    inquire
-      .prompt([
-        {
-          type: "list",
-          name: "mgrTask",
-          message: "What is your task?",
-          choices: [
-            "View Items",
-            "View Low Inventory",
-            "Add To Inventory",
-            "Add Product",
-            "Exit"
-          ]
-        }
-      ])
-      .then(function(task) {
-        if (task.mgrTask === "View Low Inventory") {
-          lowInventory().then(managerTask);
-        } else if (task.mgrTask === "Add To Inventory") {
-          displayInventory().then(addAsk);
-        } else if (task.mgrTask === "Add Product") {
-          createItem().then(newItem);
-        } else if (task.mgrTask === "Exit") {
-          connection.end();
-        } else if (task.mgrTask === "View Items") {
-          displayInventory().then(managerTask);
-        }
-      });
+function managerTask() {
+  inquire
+    .prompt([
+      {
+        type: "list",
+        name: "mgrTask",
+        message: "What is your task?",
+        choices: [
+          "View Items",
+          "View Low Inventory",
+          "Add To Inventory",
+          "Add Product",
+          "Exit"
+        ]
+      }
+    ])
+    .then(function(task) {
+      if (task.mgrTask === "View Low Inventory") {
+        lowInventory().then(managerTask);
+      } else if (task.mgrTask === "Add To Inventory") {
+        displayInventory().then(addAsk);
+      } else if (task.mgrTask === "Add Product") {
+        createItem().then(newItem);
+      } else if (task.mgrTask === "Exit") {
+        connection.end();
+      } else if (task.mgrTask === "View Items") {
+        displayInventory().then(managerTask);
+      }
+    });
 }
 
 //SELECT * FROM view products
-
 
 function displayInventory() {
   return new Promise((resolve, reject) => {
@@ -82,7 +81,7 @@ function lowInventory() {
       if (err) reject(err);
       var t = new Table();
       lowStock.forEach(function(product) {
-        if (product.quanity <= 100) {
+        if (product.quanity <= 5) {
           t.cell("Item", product.product_name);
           t.cell("Quanity", product.quanity);
           t.newRow();
@@ -94,46 +93,89 @@ function lowInventory() {
   });
 }
 
-function addAsk()  {
-  inquire.prompt([
-    {
-      type: "input",
-      name: "prodId",
-      message: "Add to ID#"
-    },
-    {
-      type: "input",
-      name: "amount",
-      message: "Add Stock"
-    }
-  ]).then(addQuanity)
+function updateDisplay(display)  {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM products WHERE id=?",[display.id] ,function(err, res) {
+      if (err) reject(err);
+
+      var data = res;
+      var t = new Table();
+
+      data.forEach(function(product) {
+        t.cell("Item ID", product.id);
+        t.cell("Item Name", product.product_name);
+        t.cell("Department Name", product.department_name);
+        t.cell("Price $", product.price);
+        t.cell("Quanity Available", product.quanity);
+        t.newRow();
+      });
+
+      console.log(t.toString());
+      resolve(res);
+    });
+  });
+}
+
+function addAsk() {
+  inquire
+    .prompt([
+      {
+        type: "input",
+        name: "prodId",
+        message: "Add to ID#"
+      },
+      {
+        type: "input",
+        name: "amount",
+        message: "Add Stock"
+      }
+    ])
+    .then(addQuanity)
+    .then(updateAdd)
+    .then(updateDisplay)
+    .then(managerTask);
 }
 //UPDATE SET WHERE id add to
 function addQuanity(val) {
-      return new Promise((resolve, reject) => {
-        connection.query("SELECT * FROM products WHERE id=?",[val.prodId], function(err,res) {
-            if(err) reject(err);
-            var add = parseFloat(val.amount);
-            var org = parseFloat(res[0].quanity);
-            
-          connection.query("UPDATE products SET ? WHERE ?",[
-            {
-              quanity: add + org
-            },  {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM products WHERE id=?",
+      [val.prodId],
+      function(err, res) {
+        if (err) reject(err);
+        var add = parseFloat(val.amount) + parseFloat(res[0].quanity);
 
-              id: val.prodId
-            }
-          ], function(err,res)  {
-            displayInventory().then(mangerTask);
-          })
-          resolve(res);
-          });
-          
-            
-          
-        });
+        var update = {
+          quanity: add,
+          id: val.prodId
+        };
+
+        resolve(update);
+      }
+    );
+  });
 }
 
+function updateAdd(update) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          quanity: update.quanity
+        },
+        {
+          id: update.id
+        }
+      ],
+      function(err, res) {
+        if (err) reject(err);
+        console.log(update)
+        resolve(update);
+      }
+    );
+  });
+}
 
 // INSERT INTO table(x,y,z,a)  VALUES (dd,ee,ss,rr) add product
 function newItem(stuff) {
@@ -161,45 +203,49 @@ function newItem(stuff) {
         message: "Receive Quanity"
       }
     ])
-    .then(updateProd);
-  }
+    .then(updateProd)
+    .then(addDisplay)
+    .then(managerTask);
+}
 
+function updateProd(post) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "INSERT INTO products SET ?",
+      {
+        product_name: post.product,
+        department_name: post.department,
+        price: post.price,
+        quanity: post.quanity
+      },
+      function(err, res) {
+        if (err) reject(err);
 
-  function updateProd(post)  {
-    return new Promise((resolve,reject) =>  {
-        connection.query("INSERT INTO products SET ?",
-        {
-            product_name: post.product,
-            department_name: post.department,
-            price: post.price,
-            quanity: post.quanity
-        }, function(err,res)  {
-            if(err) reject(err);
-            
-            displayInventory().then(managerTask);
-            resolve(post);
-              
-          })
-      
-    })
-  }
+        resolve(post);
+      }
+    );
+  });
+}
 
-  function createItem() {
-      return new Promise((resolve,reject) =>  {
-        connection.query("SELECT dpt_name FROM departments", function(err,res)  {
-          if(err) reject(err);
-          var departments = [];
-          res.forEach((product) => {
-            departments.push(product.dpt_name);
-          })  
-          resolve(departments);
-        })
-      })
-  }
+function addDisplay(stuff) {
+  var t = new Table();
+  t.cell("Product Name", stuff.product);
+  t.cell("Department", stuff.department);
+  t.cell("Price", stuff.price);
+  t.cell("Quanity Added", stuff.quanity);
+  t.newRow();
+  console.log(t.toString());
+}
 
-  
-
-
-
-
-
+function createItem() {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT dpt_name FROM departments", function(err, res) {
+      if (err) reject(err);
+      var departments = [];
+      res.forEach(product => {
+        departments.push(product.dpt_name);
+      });
+      resolve(departments);
+    });
+  });
+}
